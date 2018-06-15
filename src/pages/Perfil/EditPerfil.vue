1d08d468ea25ade938d974d1cd23bd9b331f3699
ValessioBrito <contato@valessiobrito.com.br>
2018-06-15 11:27:36 -0300

    Form Perfil

--- src/pages/Perfil/EditPerfil.vue
+++ src/pages/Perfil/EditPerfil.vue
@@ -3,73 +3,130 @@
     <md-card>
       <md-card-header :data-background-color="dataBackgroundColor">
         <h4 class="title">Informações Pessoais</h4>
-        <p class="category">Complete your profile</p>
+        <p class="category"></p>
       </md-card-header>
 
       <md-card-content>
         <div class="md-layout">
-          <div class="md-layout-item md-small-size-100 md-size-33">
+          <div class="md-layout-item md-small-size-100 md-size-100">
             <md-field>
-              <label>Company (disabled)</label>
-              <md-input v-model="disabled" disabled></md-input>
+              <label>Nome Completo</label>
+              <md-input v-model="nome" type="text"></md-input>
             </md-field>
           </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
+          <div class="md-layout-item md-small-size-100 md-size-40">
             <md-field>
-              <label>User Name</label>
-              <md-input v-model="username" type="text"></md-input>
+              <label>Nome Social</label>
+              <md-input v-model="nomesocial" type="text"></md-input>
             </md-field>
           </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
+         <div class="md-layout-item md-small-size-50 md-size-40">
+            <md-field>
+              <label>Nome Artistico</label>
+              <md-input v-model="nomeartistico" type="text"></md-input>
+            </md-field>
+          </div>
+           <div class="md-layout-item md-small-size-50 md-size-20">
+            <md-field>
+              <label>Data</label>
+              <date-picker v-model="time1" :first-day-of-week="1" width="100%" placeholder="Data de nascimento" lang="pt-br"></date-picker>
+            </md-field>
+          </div>
+         <div class="md-layout-item md-small-size-50 md-size-33">
+            <md-field>
+              <md-list-item to="#something" class="dropdown">
+                <drop-down>
+                  <a slot="title" class="dropdown-toggle" data-toggle="dropdown">
+                    <span class="label">Raça/Cor</span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="#">Pardo</a></li>
+                    <li><a href="#">Branco</a></li>
+                    <li><a href="#">Amarelo</a></li>
+                  </ul>
+                </drop-down>
+              </md-list-item>
+            </md-field>
+          </div>
+         <div class="md-layout-item md-small-size-50 md-size-33">
+            <md-field>
+              <md-list-item to="#something" class="dropdown">
+                <drop-down>
+                  <a slot="title" class="dropdown-toggle" data-toggle="dropdown">
+                    <span class="label">Orientação sexual</span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="#">Heterossexual</a></li>
+                    <li><a href="#">Homossexual</a></li>
+                    <li><a href="#">Bissexual</a></li>
+                  </ul>
+                </drop-down>
+              </md-list-item>
+            </md-field>
+          </div>
+         <div class="md-layout-item md-small-size-50 md-size-33">
+            <md-field>
+              <md-list-item to="#something" class="dropdown">
+                <drop-down>
+                  <a slot="title" class="dropdown-toggle" data-toggle="dropdown">
+                    <span class="label">Gênero</span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="#">Homem</a></li>
+                    <li><a href="#">Mulher</a></li>
+                    <li><a href="#">Transgênero</a></li>
+                  </ul>
+                </drop-down>
+              </md-list-item>
+            </md-field>
+          </div>
+         <div class="md-layout-item md-small-size-50 md-size-50">
+              <md-checkbox v-model="acessibilidade" value="Sim">Pessoa com Deficiência ou Mobilidade Reduzida? {{ acessibilidade }}</md-checkbox>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-70">
             <md-field>
               <label>Email Address</label>
               <md-input v-model="emailadress" type="email"></md-input>
             </md-field>
           </div>
-          <div class="md-layout-item md-small-size-100 md-size-50">
+          <div class="md-layout-item md-small-size-100 md-size-30">
             <md-field>
-              <label>First Name</label>
-              <md-input v-model="firstname" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
-              <label>Last Name</label>
-              <md-input v-model="lastname" type="text"></md-input>
+              <label>Telefone</label>
+              <md-input v-model="telefone" type="phone"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-100">
             <md-field>
-              <label>Adress</label>
-              <md-input v-model="address" type="text"></md-input>
+              <label>Endereço</label>
+              <md-input v-model="endereco" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-33">
             <md-field>
-              <label>City</label>
-              <md-input v-model="city" type="text"></md-input>
+              <label>Estado</label>
+              <md-input v-model="estado" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-33">
             <md-field>
-              <label>Country</label>
-              <md-input v-model="country" type="text"></md-input>
+              <label>Cidade</label>
+              <md-input v-model="cidade" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-33">
             <md-field>
-              <label>Postal Code</label>
+              <label>CEP</label>
               <md-input v-model="code" type="number"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-size-100">
             <md-field maxlength="5">
-              <label>About Me</label>
-              <md-textarea v-model="aboutme"></md-textarea>
+              <label>Anotações</label>
+              <md-textarea v-model="anotacoes"></md-textarea>
             </md-field>
           </div>
           <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-raised md-success">Update Profile</md-button>
+            <md-button class="md-raised md-success">Salvar</md-button>
           </div>
         </div>
 
@@ -78,8 +135,11 @@
   </form>
 </template>
 <script>
+import DatePicker from 'vue2-datepicker'
+
 export default {
   name: 'edit-perfil',
+  components: { DatePicker },
   props: {
     dataBackgroundColor: {
       type: String,
@@ -88,16 +148,11 @@ export default {
   },
   data () {
     return {
-      username: null,
+      acessibilidade: false,
       disabled: null,
       emailadress: null,
-      lastname: null,
-      firstname: null,
-      address: null,
-      city: null,
-      country: null,
       code: null,
-      aboutme: "Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+      anotacoes: null
     }
   }
 }
